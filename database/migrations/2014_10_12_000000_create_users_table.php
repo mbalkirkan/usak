c5e75779ad8d1de4fc63c7744bb5d8a205975da8
MBALKIRKAN <hbmemati@gmail.com>
2022-08-29 13:58:47 +0300

    news collaboration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('active')->nullable();
+            $table->double('active')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
